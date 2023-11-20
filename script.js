fe644012c34b8cf374c1380c1f42954870aa3c5e
ondrejhranicky <71215419+ondrejhranicky@users.noreply.github.com>
2023-11-20 14:57:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    ScrollReveal().reveal('.container', {
+        duration: 1000,
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        distance: '20px',
+        mobile: true
+    });
+
+    ScrollReveal().reveal('.about, .services, .gallery, .contact, footer', {
+        duration: 1000,
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        distance: '20px',
+        mobile: true
+    });
+});
